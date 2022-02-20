232b56b5704991beea74b06bfd59fc51caa0ab8f
dbtmddus <dbtmddus112@naver.com>
2022-02-20 16:06:50 +0000

    token should be refreshed on every request!(request한번마다, 다시 token을 받아와야함), test:pass

--- restAPI/restAPI.py
+++ restAPI/restAPI.py
@@ -7,34 +7,63 @@ import requests
 access_key = 'kZEmXq2ZhtYtifIxGuJquVxASfvvJv88sJwXQJxD'
 secret_key = 'wHumZzesU4S9PU6AhaDdPhZVuNMjmmcQa4bqCTmf'
 
-payload = {
-    'access_key': access_key,
-    'nonce': str(uuid.uuid4()),
-}
+payload,jwt_token,authorize_token,headers = 0,0,0,0
 
-jwt_token = jwt.encode(payload, secret_key)
-authorize_token = 'Bearer {}'.format(jwt_token)
-headers = {"Authorization": authorize_token}
+def getToken():
+    global payload,jwt_token,authorize_token,headers
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4()),
+    }
+
+    jwt_token = jwt.encode(payload, secret_key)
+    authorize_token = 'Bearer {}'.format(jwt_token)
+    headers = {"Authorization": authorize_token}
+
+def getTokenQuery(query):
+    global headers
+
+    query_string = urlencode(query).encode()
+
+    m = hashlib.sha512()
+    m.update(query_string)
+    query_hash = m.hexdigest()
+
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4()),
+        'query_hash': query_hash,
+        'query_hash_alg': 'SHA512',
+    }
+
+    jwt_token = jwt.encode(payload, secret_key)
+    authorize_token = 'Bearer {}'.format(jwt_token)
+    headers = {"Authorization": authorize_token}
 
 def getBalance():
+    getToken()
     res = requests.get('https://api.upbit.com/v1/accounts', headers=headers)
     return res.json()
 
 def getBalanceKRW():
+    getToken()
     res = requests.get('https://api.upbit.com/v1/accounts', headers=headers)
     balance = res.json()
     ret = -1
+    print("balance", balance)
     for ticker in balance:
         if (ticker['currency'] == 'KRW'):
             ret = ticker['balance']        
     return float(ret)
 
 def getInfo(ticker_list):
+    getToken()
     url = "https://api.upbit.com/v1/ticker?markets=" + ticker_list
     res = requests.request("GET", url, headers=headers)
     return res.json()
 
 def getUpbitItem():
+    getToken()
     url = "https://api.upbit.com/v1/market/all"
     res = requests.request("GET", url, headers=headers)
     return res.json()
@@ -59,7 +88,7 @@ def getAllPrice():
         ret[item["market"]] = item["trade_price"]
     return ret
     
-def buyMarketPrice(market, price):    
+def buyMarketPrice(market, price):
     query = {
         'market': market,
         'side': 'bid',
@@ -67,27 +96,14 @@ def buyMarketPrice(market, price):
         'price': str(price),
         'ord_type': 'price',
     }
-    query_string = urlencode(query).encode()
 
-    m = hashlib.sha512()
-    m.update(query_string)
-    query_hash = m.hexdigest()
-
-    payload = {
-        'access_key': access_key,
-        'nonce': str(uuid.uuid4()),
-        'query_hash': query_hash,
-        'query_hash_alg': 'SHA512',
-    }
-
-    jwt_token = jwt.encode(payload, secret_key)
-    authorize_token = 'Bearer {}'.format(jwt_token)
-    headers = {"Authorization": authorize_token}
-
+    getTokenQuery(query)
+    print("매수주문", market, "매수금액:", price)
     res = requests.post("https://api.upbit.com/v1/orders", params=query, headers=headers)
     return res.json()
 
 def sellMarketPrice(market, volume):
+    #volume == None 일 때 잔고 전체로 설정
     ticker = market.split('-')[1]
     if (volume == None):
         balance = getBalance()
@@ -103,27 +119,14 @@ def sellMarketPrice(market, volume):
 #        'price': str(price),
         'ord_type': 'market',
     }
-    query_string = urlencode(query).encode()
-
-    m = hashlib.sha512()
-    m.update(query_string)
-    query_hash = m.hexdigest()
-
-    payload = {
-        'access_key': access_key,
-        'nonce': str(uuid.uuid4()),
-        'query_hash': query_hash,
-        'query_hash_alg': 'SHA512',
-    }
-
-    jwt_token = jwt.encode(payload, secret_key)
-    authorize_token = 'Bearer {}'.format(jwt_token)
-    headers = {"Authorization": authorize_token}
-
+    
+    getTokenQuery(query)
+    print("매도주문", market, "갯수", volume)
     res = requests.post("https://api.upbit.com/v1/orders", params=query, headers=headers)
     return res.json()
 
 if __name__ == "__main__":
+    print(getBalanceKRW())
 #    print(buyMarketPrice('KRW-BTC', 10000))
 #    print(sellMarketPrice('KRW-BTC', None))
-    print(getBalanceKRW())
+#    sellMarketPrice('KRW-BTC', None)   #전량 시장가 매도
