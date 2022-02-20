6d31234af3576e8adbf0c52dd4c84e8755c5f67a
dbtmddus <dbtmddus112@naver.com>
2022-02-20 08:30:07 +0000

    add REST API function(getBalance), test:pass

--- restAPI.py
+++ restAPI.py
@@ -1,6 +1,29 @@
+import jwt
+import uuid
+import hashlib
+from urllib.parse import urlencode
 import requests
 
-url = "https://api.upbit.com/v1/ticker?markets=KRW-BTC%2C%20BTC-ETH"
-headers = {"Accept": "application/json"}
-response = requests.request("GET", url, headers=headers)
-print(response.text)
+access_key = 'kZEmXq2ZhtYtifIxGuJquVxASfvvJv88sJwXQJxD'
+secret_key = 'wHumZzesU4S9PU6AhaDdPhZVuNMjmmcQa4bqCTmf'
+
+payload = {
+    'access_key': access_key,
+    'nonce': str(uuid.uuid4()),
+}
+
+jwt_token = jwt.encode(payload, secret_key)
+authorize_token = 'Bearer {}'.format(jwt_token)
+headers = {"Authorization": authorize_token}
+
+def getBalance():
+    res = requests.get('https://api.upbit.com/v1/accounts', headers=headers)
+    print(res.json())
+
+def getInfo(ticker):
+    url = "https://api.upbit.com/v1/ticker?markets=KRW-BTC%2C%20" + ticker
+    res = requests.request("GET", url, headers=headers)
+    print(res.text)
+
+getBalance()
+getInfo("BTC-ETH")
