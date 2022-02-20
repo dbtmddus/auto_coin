6e8ef193324841f05d9152cf7fd17eb2e0b2eb95
dbtmddus <dbtmddus112@naver.com>
2022-02-20 16:50:14 +0000

    small refactoryng

--- restAPI/stretagy.py
+++ restAPI/stretagy.py
@@ -3,27 +3,25 @@ import pyupbit
 import datetime
 from restAPI import getBalance,getBalanceKRW,getInfo,getAllInfo,getAllPrice,buyMarketPrice,sellMarketPrice
 
-def checkBuy():
-    print("checkBuy")
+def checkBuyCondition():
     for market in dic:
         if market in preDic:
             prePrice = preDic[market]
             price = dic[market]
             change = ((price-prePrice)/price)*100 
-            if ( change > 0.5 ):
-                ret = buyMarketPrice(market, getBalanceKRW()/10)   #잔액의 일부로 시장가 매수
+            if ( change > 7 ):
+                ret = buyMarketPrice(market, getBalanceKRW()/2)   #잔액의 일부로 시장가 매수
                 print("buy! (market:" , market ,", " , prePrice , "->" , price , " " , change , "%)")
         else:
             print("new market!" , market)
 
-def checkSell():
-    print("checkSell")
+def checkSellCondition():
     for market in dic:
         if market in preDic:
             prePrice = preDic[market]
             price = dic[market]
             change = ((price-prePrice)/price)*100 
-            if ( change < -0.2 ):
+            if ( change < -2 ):
                 ret = sellMarketPrice(market, None)   #전량 시장가 매도
                 if (ret != None):
                     print("sell! (market:" , market ,", " , prePrice , "->" , price , " " , change , "%)")
@@ -31,7 +29,7 @@ def checkSell():
             print("error : no item!" , market)
 
 # 자동매매 시작
-print(getBalance())
+print("잔고 :", getBalance())
 dic = getAllPrice()
 preDic = dic
 
@@ -41,12 +39,12 @@ while True:
         dic = getAllPrice()
 
         #check buy&sell
-        checkBuy()
-        checkSell()
+        checkBuyCondition()
+        checkSellCondition()
 
         #finish work
         preDic = dic
-        time.sleep(1)
+        time.sleep(0.5)
     except Exception as e:
         print(e)
         time.sleep(1)
