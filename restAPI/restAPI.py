87e825c65f89c6723acbf033b38a1241c8021c23
dbtmddus <dbtmddus112@naver.com>
2022-02-20 14:50:01 +0000

    implement sell function(매도 기능 구현), test:pass

--- restAPI/restAPI.py
+++ restAPI/restAPI.py
@@ -50,9 +50,9 @@ def getAllPrice():
         ret[item["market"]] = item["trade_price"]
     return ret
     
-def buyPrice(ticker, price):    
+def buyMarketPrice(market, price):    
     query = {
-        'market': ticker,
+        'market': market,
         'side': 'bid',
 #        'volume': null,    시장가 매수는 volume 미사용
         'price': str(price),
@@ -78,7 +78,43 @@ def buyPrice(ticker, price):
     res = requests.post("https://api.upbit.com/v1/orders", params=query, headers=headers)
     return res.json()
 
+def sellMarketPrice(market, volume):
+    ticker = market.split('-')[1]
+    if (volume == None):
+        balance = getBalance()
+        for item in balance:
+            if( item['currency'] == ticker ):
+                volume = item['balance']
+                print("전량 매도", volume)
+
+    query = {
+        'market': market,
+        'side': 'ask',
+        'volume': str(volume),
+#        'price': str(price),
+        'ord_type': 'market',
+    }
+    query_string = urlencode(query).encode()
+
+    m = hashlib.sha512()
+    m.update(query_string)
+    query_hash = m.hexdigest()
+
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4()),
+        'query_hash': query_hash,
+        'query_hash_alg': 'SHA512',
+    }
+
+    jwt_token = jwt.encode(payload, secret_key)
+    authorize_token = 'Bearer {}'.format(jwt_token)
+    headers = {"Authorization": authorize_token}
+
+    res = requests.post("https://api.upbit.com/v1/orders", params=query, headers=headers)
+    return res.json()
+
 if __name__ == "__main__":
-#    print(getAllInfo())
-#    print(getAllPrice()) 
-     print(buyPrice('KRW-BTC', 10000))
+#    print(buyMarketPrice('KRW-BTC', 10000))
+    print(sellMarketPrice('KRW-BTC', None))
+    #print(getBalance())
