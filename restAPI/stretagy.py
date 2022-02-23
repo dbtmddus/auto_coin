320da40f107109359d916bdbfa0b39245a5f6b61
dbtmddus <dbtmddus112@naver.com>
2022-02-23 08:27:02 +0000

    매도 시에 currency->market으로 변경에 이슈가 있어 수정. unit_currency+cureency 가 market의 ticker와 동일하다는 보장은 없음. 수기로 BTC, KRW, USDT로 각자 market에 등록되어있는지 확인하여 market생성

--- restAPI/stretagy.py
+++ restAPI/stretagy.py
@@ -3,6 +3,11 @@ import pyupbit
 import datetime
 from restAPI import getBalance,getBalance_unit,getInfo,getAllInfo,getAllPrice,buyMarketPrice,sellMarketPrice,getOneTick
 
+# 자동매매 시작
+print("잔고 :", getBalance())
+dic = getAllPrice()
+preDic = dic
+
 def checkBuyCondition():
     for market in dic:
         if market in preDic:
@@ -11,48 +16,54 @@ def checkBuyCondition():
             diff = ((price-prePrice)/price)*100
             unit = market.split('-')[0] 
             if diff > 4:
-                amount  = getBalance_unit(unit) #매수금액
+                amount  = getBalance_unit(unit)/3 #매수금액
                 oneTick = getOneTick(market)
                 if (price-prePrice) > oneTick:
-                    ret = buyMarketPrice(market, amount)   #잔액의 일부로 시장가 매수
-                    print("buy! (market:" , market ,", " , prePrice , "->" , price , " " , diff , "%", "oneTick:", "%.20f" %oneTick, "amount:", amount, ")" )
+                    ret = buyMarketPrice(market, amount)   #시장가 매수
+                    print("buy! (market:" , market ,", " , prePrice , "->" , price , " " , diff , "%", "oneTick:", "%.20f" %oneTick, unit, "amount:", amount, ")" )
         else:
             print("new market!" , market)
 
-def checkSellCondition():
-    #잔고 list
+units = ['BTC', 'KRW', 'USDT']
+
+def getBalance_market():
     balance_list = getBalance()
-    balance_market_list = []
+    market_list = []
     for item in balance_list:
-        market = item['unit_currency'] + '-' + item['currency']
-        balance_market_list.append(market)
+        currency = item['currency']
+        market = ""
+        unit = ""
+        if (currency != 'BTC') and (currency != 'KRW') and (currency != 'USDT'):
+            for unit_candi in units:
+                market_candi = unit_candi + '-' + currency
+                if market_candi in dic:
+                    market = market_candi
+                    unit = unit_candi
+                    market_list.append(market)
+                    break
+    return market_list
 
-    for market in dic:
-        if market in preDic:
-            if market in balance_market_list:
-                if (market != 'KRW-BTC') and (market != 'BTC-USDT'):
-                    prePrice = preDic[market]
-                    price = dic[market]
-                    diff = ((price-prePrice)/price)*100 
-                    if ( diff < -0.01 ):
-                        ret = sellMarketPrice(market, None)   #전량 시장가 매도
-                        if (ret != None):
-                            print("sell! (market:" , market ,", " , prePrice , "->" , price , " " , diff , "%)")
-        else:
-            print("error : no item!" , market)
-        
-# 자동매매 시작
-print("잔고 :", getBalance())
-dic = getAllPrice()
-preDic = dic
+def checkSellCondition():
+    market_list = getBalance_market()
 
+    for market in market_list:
+        unit = market.split('-')[0]
+        currency = market.split('-')[1]
+        if (currency != 'BTC') and (currency != 'KRW') and (currency != 'USDT'):
+            prePrice = preDic[market]
+            price = dic[market]
+            diff = ((price-prePrice)/price)*100 
+            if ( diff < -0.01 ):
+                ret = sellMarketPrice(market, None)   #전량 시장가 매도
+                if (ret != None):
+                    print("sell! (market:" , market ,", " , prePrice , "->" , price , " " , diff , "%)")
 while True:
     try:
         #pre work
         dic = getAllPrice()
 
         #check buy&sell
-        checkBuyCondition()
+#        checkBuyCondition()
         checkSellCondition()
 
         #finish work
